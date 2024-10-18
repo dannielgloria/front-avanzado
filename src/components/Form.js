97606454e00426ddadb96cbbf5fe1fffa42bb7db
dannielgloria <danniel.gloria@gmail.com>
2024-10-17 21:23:58 -0600

    update Form.js

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,13 +1,36 @@
 import React from "react";
-import { Box, Button, Grid2, TextField, Typography } from "@mui/material";
+import { Box, Button, Grid2, MenuItem, TextField, Typography } from "@mui/material";
 import useForm from "../hooks/useForm"
 
 const Form = () =>{
-    const [formData, handleChange, resetForm] = useForm({ name: '', email: '', date:'', country:'', age:''});
+    const [formData, handleChange, resetForm] = useForm({ 
+        name:    '', 
+        email:   '', 
+        date:    '', 
+        country: '', 
+        age:     '',
+    });
 
     const handleSubmint = (e) =>{
         e.preventDefault();
-        console.log(formData);
+
+        if (!/\S+@\S+.\S+/.test(formData.email)) {
+            alert("Por favor ingresa un correo válido.");
+            return;
+        }
+
+        const age = parseInt(formData.age)
+        if (isNaN(age) || age < 0 || age > 150){
+            alert("La edad debe estar entre 0 y 150");
+            return;
+        }
+        
+        if (age < 18){
+            alert("Usted esta chiquit@ no puede usar este formulario, ve a mimir (dormir)")
+            return;
+        }
+
+        console.log("Datos del formulario: ", formData);
         resetForm();
     };
 
@@ -32,6 +55,7 @@ const Form = () =>{
                         <TextField 
                             label="Correo"
                             name="email"
+                            type="email"
                             value={formData.email}
                             onChange={handleChange}
                             fullWidth
@@ -42,26 +66,35 @@ const Form = () =>{
                         <TextField 
                             label="Fecha"
                             name="date"
+                            type="date"
                             value={formData.date}
                             onChange={handleChange}
                             fullWidth
+                            InputLabelProps={{ shrink: true}}
                             required
                         />
                     </Grid2>
                     <Grid2 item xs={12}>
-                        <TextField 
-                            label="Pais"
-                            name="country"
-                            value={formData.country}
-                            onChange={handleChange}
-                            fullWidth
-                            required
-                        />
+                    <TextField
+                        select
+                        label="País"
+                        name="country"
+                        value={formData.country}
+                        onChange={handleChange}
+                        fullWidth
+                        required
+                        >
+                            <MenuItem value="Mexico">México</MenuItem>
+                            <MenuItem value="USA">USA</MenuItem>
+                            <MenuItem value="Canada">Canadá</MenuItem>
+                            <MenuItem value="Argentina">Argentina</MenuItem>
+                            </TextField>
                     </Grid2>
                     <Grid2 item xs={12}>
                         <TextField 
                             label="Edad"
                             name="age"
+                            type="number"
                             value={formData.age}
                             onChange={handleChange}
                             fullWidth
